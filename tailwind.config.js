622f09914b796c60af8022c1463d3f9e08bda704
Jessica <osalorenzo@gmail.com>
2021-11-30 22:27:36 -0500

    Initial Push

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      margin: {
+        '104': "45rem"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
