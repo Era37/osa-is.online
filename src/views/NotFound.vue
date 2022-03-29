f08c5ff9ab467df764504e03cc24dbbc10b4b28e
Era37 <osalorenzo@gmail.com>
2022-03-29 14:40:18 -0400

    adding 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="text-white">
-      Not Found
-  </div>
+	<div class="space-y-6 h-screen bg-black p-16">
+		<div class="text-4xl md:text-6xl font-bold">
+			Uh oh, seems like you're lost
+		</div>
+		<div class="">
+			<router-link to="/">
+				<button class="rounded-lg outline ouline-2 outline-white p-2 ease-out transition hover:-translate-y-1">
+					Return Home
+				</button>
+			</router-link>
+		</div>
+	</div>
 </template>
 
 <script>
