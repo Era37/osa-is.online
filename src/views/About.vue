d1ebaaedd9b69114421429c3165502f8f654eb79
Jessica <osalorenzo@gmail.com>
2022-03-28 23:03:54 -0400

    adding about me

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="text-white">
-        About Me!!
+    <div class="text-white flex">
+        <div class="mx-auto mt-44 text-center space-y-4">
+            <div class="text-8xl font-bold">About <div class="inline-flex text-transparent bg-clip-text bg-gradient-to-br from-green-200 via-green-500 to-green-900">me</div>.</div>
+            <p class="max-w-lg text-xl text-left">Hey! My name is Jessica, I'm a trans girl from Canada. I like to write Python, JavaScript and play Rainbow Six Seige. I'm 16 years old and attend highschool. Below I have listed a few of the projects which I lead</p>
+        </div>
     </div>
 </template>
 
@@ -9,7 +12,3 @@ export default {
     name: "About"
 }
 </script>
-
-<style>
-
-</style>
