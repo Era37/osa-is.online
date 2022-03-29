9f04effc6f4a3bc5a49dc76a47dad1fb0faa1559
Jessica <osalorenzo@gmail.com>
2022-03-28 23:12:46 -0400

    more css

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
     <div class="text-white flex">
         <div class="mx-auto mt-44 text-center space-y-4">
             <div class="text-8xl font-bold">About <div class="inline-flex text-transparent bg-clip-text bg-gradient-to-br from-green-200 via-green-500 to-green-900">me</div>.</div>
-            <p class="max-w-lg text-xl text-left">Hey! My name is Jessica, I'm a trans girl from Canada. I like to write Python, JavaScript and play Rainbow Six Seige. I'm 16 years old and attend highschool. Below I have listed a few of the projects which I lead</p>
+            <p class="max-w-lg text-xl text-left">Hey! My name is Jessica, I'm a trans girl from Canada. I'm a fullstack developer who works with Python, Node.js, Vue, Tailwind and much more. I play Rainbow Six Seige in my free time and love to paint my nails! You can find some of the project which I work on <router-link to="/" class="text-blue-300 hover:text-blue-500">here</router-link>.</p>
         </div>
     </div>
 </template>
