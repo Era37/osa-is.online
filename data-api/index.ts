1e7cfe13fbd7f68768b453a3ac5457bd41a30359
Jessica <osalorenzo@gmail.com>
2022-10-10 10:41:02 -0400

    Update index.ts

--- data-api/index.ts
+++ data-api/index.ts
@@ -2,7 +2,7 @@ import Fastify from "fastify";
 import database from "./utils/database";
 import dotenv from "dotenv";
 import cors from "@fastify/cors";
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: "../.env" });
 
 const server = Fastify({ logger: true });
 const origin = process.env.SPACE === 'dev' ? true : ["https://jessica-is.gay"];
@@ -17,9 +17,10 @@ server.register(require("./routes/spotify"));
 
 // Listening on port 8081, callback starts database connection
 server.listen({ port: 80, host: "0.0.0.0" }, async () => {
+    const { MONGO, REDIS } = process.env;
     await database.start({
-        mongoURL: String(process.env.MONGO),
-        redisURL: String(process.env.REDIS),
+        mongoURL: String(MONGO),
+        redisURL: String(REDIS),
     });
     console.log("API is online! " + new Date(Date.now()));
 });
