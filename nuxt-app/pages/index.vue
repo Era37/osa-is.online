ac3cb6cfc30204304fefd467d502a937dad05430
Jessica <osalorenzo@gmail.com>
2022-10-28 18:35:15 -0400

    switching twitter link to reddit

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -11,12 +11,7 @@
       <div class="text-5xl font-extrabold">
         Hey, I'm
         <span
-          class="
-            text-transparent
-            bg-clip-text bg-gradient-to-br
-            from-pink-400
-            to-red-600
-          "
+          class="text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600"
           >Jessica</span
         >.
       </div>
@@ -30,22 +25,16 @@
       </div>
       <ul class="pl-2 flex space-x-4 pt-2">
         <li class="cursor-pointer">
-          <a target="_blank" href="https://twitter.com/Era3037"
+          <a target="_blank" href="https://www.reddit.com/user/Era3037/"
             ><svg
               xmlns="http://www.w3.org/2000/svg"
-              class="
-                hover:fill-salmon
-                ease-out
-                duration-300
-                fill-white
-                hover:-translate-y-1
-              "
-              width="26"
-              height="31"
+              width="27"
+              height="27"
               viewBox="0 0 24 24"
+              class="fill-white hover:-translate-y-1 transition-all hover:fill-salmon"
             >
               <path
-                d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+                d="M24 11.779c0-1.459-1.192-2.645-2.657-2.645-.715 0-1.363.286-1.84.746-1.81-1.191-4.259-1.949-6.971-2.046l1.483-4.669 4.016.941-.006.058c0 1.193.975 2.163 2.174 2.163 1.198 0 2.172-.97 2.172-2.163s-.975-2.164-2.172-2.164c-.92 0-1.704.574-2.021 1.379l-4.329-1.015c-.189-.046-.381.063-.44.249l-1.654 5.207c-2.838.034-5.409.798-7.3 2.025-.474-.438-1.103-.712-1.799-.712-1.465 0-2.656 1.187-2.656 2.646 0 .97.533 1.811 1.317 2.271-.052.282-.086.567-.086.857 0 3.911 4.808 7.093 10.719 7.093s10.72-3.182 10.72-7.093c0-.274-.029-.544-.075-.81.832-.447 1.405-1.312 1.405-2.318zm-17.224 1.816c0-.868.71-1.575 1.582-1.575.872 0 1.581.707 1.581 1.575s-.709 1.574-1.581 1.574-1.582-.706-1.582-1.574zm9.061 4.669c-.797.793-2.048 1.179-3.824 1.179l-.013-.003-.013.003c-1.777 0-3.028-.386-3.824-1.179-.145-.144-.145-.379 0-.523.145-.145.381-.145.526 0 .65.647 1.729.961 3.298.961l.013.003.013-.003c1.569 0 2.648-.315 3.298-.962.145-.145.381-.144.526 0 .145.145.145.379 0 .524zm-.189-3.095c-.872 0-1.581-.706-1.581-1.574 0-.868.709-1.575 1.581-1.575s1.581.707 1.581 1.575-.709 1.574-1.581 1.574z"
               />
             </svg>
           </a>
@@ -56,13 +45,7 @@
             href="https://discordhub.com/profile/768615938055471116"
             ><svg
               xmlns="http://www.w3.org/2000/svg"
-              class="
-                hover:fill-salmon
-                ease-out
-                duration-300
-                fill-white
-                hover:-translate-y-1
-              "
+              class="hover:fill-salmon ease-out duration-300 fill-white hover:-translate-y-1"
               height="30"
               width="30"
               viewBox="0 0 127.14 96.36"
@@ -88,13 +71,7 @@
           <a target="_blank" href="https://github.com/Era37"
             ><svg
               xmlns="http://www.w3.org/2000/svg"
-              class="
-                hover:fill-salmon
-                ease-out
-                duration-300
-                fill-white
-                hover:-translate-y-1
-              "
+              class="hover:fill-salmon ease-out duration-300 fill-white hover:-translate-y-1"
               width="30"
               height="30"
               viewBox="0 0 24 24"
@@ -110,13 +87,7 @@
             target="_blank"
             href="https://open.spotify.com/user/vvknrxqpb2g7ql3dx3l6m09ro"
             ><svg
-              class="
-                hover:fill-salmon
-                ease-out
-                duration-300
-                hover:-translate-y-1
-                fill-white
-              "
+              class="hover:fill-salmon ease-out duration-300 hover:-translate-y-1 fill-white"
               width="30"
               height="30"
               viewBox="0 0 24 24"
@@ -134,12 +105,7 @@
       <div class="font-extrabold text-xl pt-4">
         My
         <span
-          class="
-            text-transparent
-            bg-clip-text bg-gradient-to-br
-            from-amber-500
-            to-orange-500
-          "
+          class="text-transparent bg-clip-text bg-gradient-to-br from-amber-500 to-orange-500"
           >Favorite</span
         >
         Songs
@@ -159,12 +125,7 @@
       <div class="text-5xl font-extrabold mx-auto">
         My
         <span
-          class="
-            text-transparent
-            bg-clip-text bg-gradient-to-br
-            from-lime-400
-            to-green-500
-          "
+          class="text-transparent bg-clip-text bg-gradient-to-br from-lime-400 to-green-500"
           >Blogs</span
         >.
       </div>
