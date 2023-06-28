bb1e9c4d827c2b61cc0eec80fbe9bfaf4e12ec6f
Jessica <osalorenzo@gmail.com>
2023-06-28 02:58:18 -0400

    bye bye go

--- backend/src/utils/cache.rs
+++ backend/src/utils/cache.rs
@@ -1,14 +1,22 @@
 use super::database::Db;
 use bson::from_bson;
 use futures::TryStreamExt;
-use mongodb::{Collection, bson::Document, bson::Bson};
+use mongodb::{bson::Bson, bson::Document, Collection};
 use r2d2::PooledConnection;
 use r2d2_redis::{redis::Commands, RedisConnectionManager};
-use serde_json::to_string;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
+use serde_json::to_string;
 
-pub async fn cache_data<'a, T>(key: String, filter: impl Into<Option<Document>>, database: &'a Db, collection: &str) -> Option<Vec<T>> where T: DeserializeOwned + Serialize + std::fmt::Debug {
+pub async fn cache_data<'a, T>(
+    key: String,
+    filter: impl Into<Option<Document>>,
+    database: &'a Db,
+    collection: &str,
+) -> Option<Vec<T>>
+where
+    T: DeserializeOwned + Serialize + std::fmt::Debug,
+{
     let mut redis_conn: PooledConnection<RedisConnectionManager> = database.redis.get().unwrap();
     let value: Option<String> = redis_conn.get(&key).ok();
     if value != None {
@@ -25,7 +33,9 @@ pub async fn cache_data<'a, T>(key: String, filter: impl Into<Option<Document>>,
             let converted_entry: T = from_bson(Bson::Document(entry.unwrap())).unwrap();
             database_data.push(converted_entry);
         }
-        let _ : () = redis_conn.set(&key, to_string(&database_data).unwrap()).unwrap();
+        let _: () = redis_conn
+            .set(&key, to_string(&database_data).unwrap())
+            .unwrap();
         let _: () = redis_conn.expire(&key, 300).unwrap();
         return Some(database_data);
     }
