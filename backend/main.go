2bb2f4e72555d14570a7cc15d6787b6fea312af0
Jessica <osalorenzo@gmail.com>
2023-09-14 23:19:26 -0400

    adding some basic drivers, laying ground work

--- backend/main.go
+++ backend/main.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	//"database/sql"
+
 	"fmt"
 	"net/http"
 
 	"jessica-is.online/backend/utils"
 )
+var db utils.Database
 
 func home(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Pg.Query("SELECT * FROM blogs")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for rows.Next() {
+		
+	}
 	fmt.Fprintf(w, "hello :3")
 }
 
 func main() {
 	http.HandleFunc("/", home)
 	fmt.Println("Online")
-	utils.Connect()
+	db = utils.Connect()
 	err := http.ListenAndServe("0.0.0.0:80", nil)
 	if err != nil {
 		fmt.Println(err)
--- backend/utils/database.go
+++ backend/utils/database.go
@@ -1,5 +1,23 @@
 package utils
 
-func Connect() {
+import (
+	"database/sql"
+	"log"
 
+	_ "github.com/lib/pq"
+	"github.com/redis/go-redis/v9"
+)
+
+type Database struct {
+	Redis *redis.Client
+	Pg *sql.DB
+}
+
+func Connect() Database {
+	redis := redis.NewClient(&redis.Options{})
+	pg, err := sql.Open("postgres", "postgresql://postgres:a@0.0.0.0:5432?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return Database{redis, pg}
 }
