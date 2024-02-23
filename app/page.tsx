0950f791f05602630883ea7a8d136f49718a20b3
Jessica <osalorenzo@gmail.com>
2024-02-23 07:36:52 -0500

    Removing projects. Adding spot for dedicated email button on site. Will use backend SMTP

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import IntroTitle from "@/components/IntroTItle";
 import Project from "@/components/Project";
 import SocialAccounts from "@/components/SocialAccounts";
 import Subhead from "@/components/Subhead";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,7 +14,14 @@ export default function Home() {
         <div className="mt-6 text-lg tracking-wide">
           My name is Osa, and I'm a student from Ontario Canada. I hobby as a
           fullstack developer who works with Python, Node.js, Vue, Tailwind,
-          Rust, C(++) and much more.
+          Rust, C(++) and much more. To view my projects checkout my{" "}
+          <Link
+            className="font-bold flex"
+            target="_blank"
+            href="https://github.com/era37"
+          >
+            Github
+          </Link>
         </div>
         <Subhead>Blogs</Subhead>
         <Blog
@@ -21,19 +29,6 @@ export default function Home() {
           description="this is a test meow meow meow I like women asdkfjnsd"
           date={new Date(Date.now())}
         />
-        <Subhead>Projects</Subhead>
-        <div className="pt-4 flex space-x-4">
-          <Project
-            title="Xiana"
-            description="Xiana is an open source media storage system included with a CLI for managing your media storage in production"
-            className="hover:border-xiana hover:text-xiana"
-          />
-          <Project
-            title="tifi"
-            className="hover:border-tifi hover:text-tifi"
-            description="tifi is a image building platform with the ability to publish media and blogs."
-          />
-        </div>
         <Subhead>Contact</Subhead>
         <div className="mt-4 mb-24 flex space-x-4">
           <Button className="bg-white text-black" href="test">
