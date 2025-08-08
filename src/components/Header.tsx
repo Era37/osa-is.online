633b58c6ce9c50e0bbb83a060843fd5e1ab1a250
Osa <osalorenzo@gmail.com>
2025-08-08 14:45:05 -0400

    minor UI updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,24 +29,23 @@ export default function ThemeToggleBar() {
 
   return (
     <div className="mt-10 absolute">
-      <div className="fixed h-10 z-10 inset-0 backdrop-blur-md w-screen"></div>
-      <div className="fixed -translate-y-5 blur w-screen h-15 backdrop-blur-md z-20"></div>
-      <div className="bg-black dark:bg-white p-2 flex z-30 text-white dark:text-black sm:w-[75vw] md:w-[50vw] rounded-full left-1/2 -translate-x-1/2 space-x-4 my-box fixed">
+      <div className="fixed h-32 z-10 top-0 left-0 right-0 bg-gradient-to-b from-white/20 via-white/10 to-transparent dark:from-black/20 dark:via-black/10 dark:to-transparent backdrop-blur-[2px]"></div>
+      <div className="bg-black dark:bg-white p-4 flex z-30 text-white dark:text-black sm:w-[75vw] md:w-[55vw] lg:w-[45vw] rounded-2xl left-1/2 -translate-x-1/2 space-x-6 my-box fixed items-center">
         <div
           onClick={toggleShift}
-          className="z-10 relative cursor-pointer pr-4"
+          className="z-10 relative cursor-pointer pr-3"
         >
-          <div className="h-5 w-5 rounded-full bg-white dark:bg-black top-1/2 absolute -translate-y-1/2"></div>
+          <div className="h-8 w-8 rounded-full bg-white dark:bg-black top-1/2 absolute -translate-y-1/2"></div>
           <div
-            className={`h-5 w-5 rounded-full dark:bg-white bg-black top-1/2 absolute -translate-y-3 transition-transform duration-300 ${
-              isShifted ? "translate-x-5" : "translate-x-1.5"
+            className={`h-8 w-8 rounded-full dark:bg-white bg-black top-1/2 absolute -translate-y-4 transition-transform duration-300 ${
+              isShifted ? "translate-x-8" : "translate-x-2"
             }`}
           ></div>
         </div>
-        <div className="flex space-x-6 m-auto pr-6 font-bold z-40 relative sm:text-md text-sm ">
+        <div className="flex space-x-8 m-auto pr-4 font-bold z-40 relative text-lg sm:text-xl audiowide">
           {tabs.map((tab, i) => (
             <a
-              className="transition-all hover:-translate-y-0.5 cursor-pointer duration-500"
+              className="transition-all hover:-translate-y-0.5 hover:text-gray-300 dark:hover:text-gray-600 cursor-pointer duration-300"
               key={i}
               href={`#${tab.toLowerCase()}`}
             >
@@ -58,7 +57,7 @@ export default function ThemeToggleBar() {
       {/* Custom shadow styling */}
       <style>{`
         .my-box {
-          box-shadow: 0px 0px 50px rgba(0, 0, 0, 1);
+          box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.3);
         }
       `}</style>
     </div>
