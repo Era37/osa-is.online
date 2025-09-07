b62f2a01752f0858e8cc17769abee1eac9b640ce
Osa George-Mannella <osalorenzo@gmail.com>
2025-09-06 21:19:34 -0400

    Writing a blog

--- src/lib/blogs.ts
+++ src/lib/blogs.ts
@@ -19,7 +19,18 @@ export async function getBlog(
   return response.text();
 }
 
-export const blogs: BlogPost[] = [];
+export const blogs: BlogPost[] = [
+  {
+    id: "writing-my-compiler",
+    title: "Why I Decided to Design My Own Language",
+    description:
+      "A reflection on why I designed n, a compact language for building REST APIs that borrows from functional programming without sacrificing pragmatism.",
+    slug: "writing-my-compiler",
+    date: new Date(2025, 8, 6),
+    readTime: "5 min read",
+    tags: [],
+  },
+];
 
 export function getBlogBySlug(slug: string): BlogPost | undefined {
   return blogs.find((blog) => blog.slug === slug);
